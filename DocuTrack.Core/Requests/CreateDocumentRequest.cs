3a117d9aa7bdf9f1238b4e75a9e5197aed6c6e16
Vaishnavi Bachhav <vaishnavi.bachhav111@gmail.com>
2026-07-23 18:22:12 -0400

    Phase 1 - Console application created

--- DocuTrack.Core/Requests/CreateDocumentRequest.cs
+++ DocuTrack.Core/Requests/CreateDocumentRequest.cs
@@ -1,7 +1,4 @@
 ﻿using DocuTrack.Core.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DocuTrack.Core.Requests
 {
@@ -11,7 +8,7 @@ namespace DocuTrack.Core.Requests
         public string? Description { get; init; }
         public DocumentType DocumentType { get; init; }
         public Department Department { get; init; }
-        public required string Owner { get; init; } 
+        public required string Owner { get; init; }
 
         // Using init here -- prevents modification after the request is created.
     }
